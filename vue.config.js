efce9e1b5f7d4c47c1b6a690a3575dc96aec86b1
002 <wq002@everelite-zdeMacBook-Pro.local>
2019-11-11 16:00:12 +0800

    1

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    //输出文件目录
+    publicPath: './',
+    // 打包文件目录
+    outputDir:'dist',
+    // 静态资源文件目录
+    assetsDir:'static',
+    // 服务设置
+    devServer:{
+        open:'true',
+        host:'127.0.0.1',
+        port:'8000'
+    }
+}
