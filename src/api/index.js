8230b93e2a5aed578f554eb9ed78b2b8ae3e187a
flyhero <qfwang666@163.com>
2019-12-09 19:58:12 +0800

    feat:增加API

--- src/api/index.js
+++ src/api/index.js
@@ -1,38 +1,53 @@
 // 统一请求路径前缀在libs/axios.js中修改
 import {
-  getRequest,
-  postRequest,
-  putRequest,
-  patchRequest,
-  deleteRequest,
-  getRequestWithNoToken
+	getRequest,
+	postRequest,
+	putRequest,
+	patchRequest,
+	deleteRequest,
+	getRequestWithNoToken
 } from "@/libs/axios";
 
 // 登陆
 export const login = params => {
-  return postRequest("/users/login", params);
+	return postRequest("/users/login", params);
 };
 // 注册
 export const regist = params => {
-  return postRequest("/users/register", params);
+	return postRequest("/users/register", params);
 };
 
 // 查询指定用户
 export const userInfo = (userId) => {
-  return getRequest("/v1/users/"+userId);
+	return getRequest("/v1/users/" + userId);
 };
 
 // 更新指定用户信息
 export const userInfoEdit = (userId, params) => {
-  return patchRequest("/v1/users/${userId}", params);
+	return patchRequest("/v1/users/" + userId, params);
 };
 
 // 问题答案文件上传
 export const uploadQusetion = params => {
-  return postRequest("/files/questions", params);
+	return postRequest("/files/questions", params);
 };
 
 //查询题目
 export const search = params => {
-  return getRequest("/v1/questions", params);
+	return getRequest("/v1/questions", params);
+};
+
+//查询题目详情
+export const questionDetail = (questionId) => {
+	return getRequest("/v1/questions/" + questionId);
+};
+
+// 添加题目
+export const addQuestion = (params) => {
+	return postRequest("/v1/questions/", params);
+};
+
+// 查询所有标签
+export const getLabels = params => {
+	return getRequest("/v1/labels", params);
 };
